f94c907eda72f6fe91ac3d3c0839c716a3cd5b1a
Dharm Kumar <dharmsharma201@gmail.com>
2025-12-23 18:47:17 +0530

    Initial commit

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -334,7 +334,7 @@ export default function DynamicProfilePage() {
                 className="px-4 py-2 border border-gray-300 rounded-lg hover:bg-gray-50 flex items-center"
               >
                 <Printer className="h-4 w-4 mr-2" />
-                Print
+                Prints
               </button>
               <button
                 onClick={() => router.push(userType === 'candidate' ? '/candidates/dashboard' : '/institutes/dashboard')}
